38ee42947e0b58bee0c228d818f54d9bf66a4809
gayeongX0916 <gayeongx0916@gmail.com>
2025-06-05 18:38:01 +0900

    [feature] 기본 레이아웃 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
-    <>
-     
-    </>
-  )
+    <div className="movie-app">
+      <header className="movie-header">
+        <div className="movie-header-title">
+          <h3>Movie</h3>
+          <h3>View</h3>
+        </div>
+        <input placeholder="Search..." />
+      </header>
+      <div className="movie-list">영화 목록</div>
+    </div>
+  );
 }
 
-export default App
+export default App;
