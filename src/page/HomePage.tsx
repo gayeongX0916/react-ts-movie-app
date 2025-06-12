4c08780753461c20ec84657a35c6c763dcd99c85
gayeongX0916 <gayeongx0916@gmail.com>
2025-06-12 16:56:05 +0900

    [feature] 로딩 및 에러 상태 관리

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -14,8 +14,22 @@ const HomePage = () => {
 
   const navigate = useNavigate();
 
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
   useEffect(() => {
-    fetchMovieData(query).then((data) => setMovies(data));
+    fetchMovieData(query)
+      .then((data) => {
+        setLoading(true);
+        setError(null);
+        setMovies(data);
+      })
+      .catch(() => {
+        setError("영화를 가져오는 중 오류 발생");
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   }, [query]);
 
   const handleSearch = async (e: KeyboardEvent<HTMLInputElement>) => {
@@ -43,6 +57,11 @@ const HomePage = () => {
           onKeyDown={(e) => handleSearch(e)}
         />
       </header>
+      {loading && <p className="movie-message">데이터를 가져오고 있습니다.</p>}
+      {error && <p className="movie-message">{error}</p>}
+      {!loading && !error && movies.length === 0 && (
+        <p className="movie-message">검색 결과가 없습니다.</p>
+      )}
       <MovieList movies={movies} />
     </div>
   );
